bf8f56cf12e8dcede3c30b32c81b09d73d23d9dd
Henrik Nilsson Harnert <henrik.nilsson.harnert@gmail.com>
2015-12-08 16:26:59 +0100

    Added structure for Ltu-Wall-Helper

--- src/ltu-wall/helper/ltu-wall-helper.h
+++ src/ltu-wall/helper/ltu-wall-helper.h
@@ -3,12 +3,38 @@
 #define LTU_WALL_HELPER_H
 
 #include "ns3/ltu-wall.h"
+#include <ns3/object.h>
 
 namespace ns3 {
 
-/* ... */
+class LtuWallHelper
+{       
+public:
+  
+  /** 
+   * Constructor
+   */
+  LtuWallHelper ();
 
-}
+  /** 
+   * Destructor
+   */  
+  ~LtuWallHelper ();
+  
+  // inherited from Object
+  /**
+   *  Register this type.
+   *  \return The object TypeId.
+   */
+  //static TypeId GetTypeId (void);
+  
+  void CreateWall (uint64_t x1, uint64_t y1, uint64_t z1, uint64_t x2, uint64_t y2, uint64_t z2);
+};
+
+
+
+
+} // namespace ns-3
 
 #endif /* LTU_WALL_HELPER_H */
 
