1971abfd6d6d963fd9c990f687d25c71a4a3e277
DavidRutqvist <david.rutqvist@bnearit.se>
2015-12-08 14:23:06 +0100

    Added some methods to the header of the Ltu-wifi-helper

--- src/ltu-wifi/helper/ltu-wifi-helper.cc
+++ src/ltu-wifi/helper/ltu-wifi-helper.cc
@@ -1,11 +1,41 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 #include "ltu-wifi-helper.h"
-
+#include "ns3/log.h"
 namespace ns3 {
 
-/* ... */
+NS_LOG_COMPONENT_DEFINE ("LtuWifiHelper");
 
+LtuWifiHelper::~LtuWifiHelper()
+{
+    //Destructor
+}
+
+NodeContainer
+LtuWifiHelper::CreateAccessPoint(std::string ssid, int x, int y, int z)
+{
+    //TODO: Fix, below just to be able to compile
+    //Create access point and return
+    NodeContainer node;
+    node.Create (1);
+    return node;
+}
+
+NodeContainer
+LtuWifiHelper::CreateClient(int x, int y, int z)
+{
+    //TODO: Fix, below just to be able to compile
+    //Create wifi client and return
+    NodeContainer node;
+    node.Create (1);
+    return node;
+}
+
+void
+LtuWifiHelper::ConnectClientToWirelessNetwork(NodeContainer client, std::string ssid)
+{
+    //Connect the supplied client (node) to the provided SSID.
+}
 
 }
 
--- src/ltu-wifi/helper/ltu-wifi-helper.h
+++ src/ltu-wifi/helper/ltu-wifi-helper.h
@@ -3,10 +3,19 @@
 #define LTU_WIFI_HELPER_H
 
 #include "ns3/ltu-wifi.h"
+#include "ns3/node-container.h"
+#include <string>
 
 namespace ns3 {
 
-/* ... */
+class LtuWifiHelper {
+public:
+    ~LtuWifiHelper ();//Destructor
+
+    NodeContainer CreateAccessPoint(std::string ssid, int x, int y, int z);
+    NodeContainer CreateClient(int x, int y, int z);
+    void ConnectClientToWirelessNetwork(NodeContainer client, std::string ssid);
+};
 
 }
 
