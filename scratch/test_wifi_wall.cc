cdf12c3fe99776384c1c3a74790eec72b16c8dbe
henriknh <henrik.nilsson.harnert@gmail.com>
2016-03-10 11:26:24 +0100

    Update test_wifi_wall.cc

--- scratch/test_wifi_wall.cc
+++ scratch/test_wifi_wall.cc
@@ -33,13 +33,22 @@
 #include <ns3/ltu-wifi.h>
 
 
-
+/*
+Example that configues 2 APs on a line and connects them with a wire. 
+Then creates a wall that most of the time block the signal between APs 
+and 2 clients. Clients move around in a area around its spawn point.
+        AP ------------------- AP               AP  Access Point
+                                 \              =   Wall
+        ========================= \             C   Client
+                                   \            \   Signal between AP and Client
+          C                         C
+*/
 
 
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("LeifiTestar");
+NS_LOG_COMPONENT_DEFINE ("Test_wifi_wall");
 
 int 
 main (int argc, char *argv[])
@@ -52,7 +61,7 @@ main (int argc, char *argv[])
 
   if (verbose)
   {
-    LogComponentEnable("LeifiTestar", LOG_LEVEL_INFO);
+    LogComponentEnable("Test_wifi_wall", LOG_LEVEL_INFO);
     LogComponentEnable("WifiAccessPoint", LOG_LEVEL_INFO);
     LogComponentEnable("WiredConnection", LOG_LEVEL_INFO);
     LogComponentEnable("WiredConnectionContainer", LOG_LEVEL_INFO);
@@ -60,29 +69,51 @@ main (int argc, char *argv[])
     LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
   }
 
+
+
+
+
   // Instantiate LtuWallHelper 
   LtuWallHelper wallhelper;
-
-  
-  wallhelper.CreateWall(-100, 5, 100, 5);
-
+  // Create a wall
+  wallhelper.CreateWall(-100, 5, 100, 5); 
+  // Installs walls in LossModel
   wallhelper.InstallWalls();
 
+
+
+
+
+
+  // Instantiate LtuWifiHelper 
   LtuWifiHelper wifi;
   wifi.SetSsid("GruvNET");//Optional
 
-
+  // Create 2 APs
   Ptr<WifiAccessPoint> ap1 = wifi.CreateAccessPoint(0, 0, 1);
   Ptr<WifiAccessPoint> ap2 = wifi.CreateAccessPoint(100, 0, 1);
+  // Connect them by a wire
   ap1->ConnectWired(ap2);
 
+  // Create 2 clients that operate on the other side of the wall
+  // They are moving. Most of the time they are in the shadow of 
+  // the wall but sometimes it will move outside of the shadow.
   NodeContainer wifiClients = wifi.CreateClient(25, 25, 1, 50, 50, 10);
   wifiClients.Add(wifi.CreateClient(100, 25, 1, 50, 50, 5));
 
-  //wifi.InstallAll();//Without building
-  wifi.InstallAll(wallhelper.GetLossModel());//With wall
+  // Choose one of the 2 wifi.InstallAll-functions. 
+  // Comment the line one you dont want.
+  //wifi.InstallAll(); // Installs Wifi without wall
+  wifi.InstallAll(wallhelper.GetLossModel()); // Installs Wifi with wall. 
+                                              // Get HybridWallPropagationLossModel from LtuWallHelper
 
 
+
+
+
+
+
+  // Setup for networking
   Address dest;
   std::string protocol;
   dest = InetSocketAddress (wifi.GetWifiClientIP(1), 1025);
@@ -90,9 +121,11 @@ main (int argc, char *argv[])
 
   OnOffHelper onoff = OnOffHelper (protocol, dest);
   onoff.SetConstantRate (DataRate ("500kb/s"));
+
+  // Setup of Simulator and runs it
   ApplicationContainer apps = onoff.Install (wifiClients.Get(0));
   apps.Start (Seconds (0.5));
-  apps.Stop (Seconds (19.5));
+  apps.Stop (Seconds (60.0));
 
   AnimationInterface anim ("wall-animation.xml");
 
